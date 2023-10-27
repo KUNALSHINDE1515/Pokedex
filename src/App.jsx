ce619fe1a81866ad73d472b8041df7b9891c6c34
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2023-10-27 13:31:05 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
-import './App.css' 
+import { useState } from "react";
+import AddTodo from "./components/AddTodo/AddTodo"
+import TodoList from "./components/TodoList/TodoList"
 
 function App(){
+  const [list,setList] = useState([
+    {id: 1,todoData:'todo1', finished:false},
+    {id: 2,todoData:'todo2',finished:true},
+]);
 
 
   return(
     <>
-    todo
+       <AddTodo updateList={(todo) => setList([
+        ...list,{id: list.length+1 , list,todoData:todo, finished:false}
+        ])}/>
+       <TodoList list={list} />
     </>
   )
 }
